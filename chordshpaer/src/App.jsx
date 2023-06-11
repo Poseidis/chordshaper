2f20754c2127b4a3d767ac4441ddf90175b9e5c0
Vincent Wei <vincent.c.wei@gmail.com>
2023-06-10 17:37:43 -0700

    vite test

---
+++ chordshpaer/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+import { MantineProvider, Text } from '@mantine/core'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Text>Welcome to Mantine!</Text>
+    </div>
+  )
+};
+
+export default App
